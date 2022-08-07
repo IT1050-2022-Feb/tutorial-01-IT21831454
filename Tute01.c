c8d47246df75ef9178c5450a36085c03949d958c
IT21831454 <110684306+IT21831454@users.noreply.github.com>
2022-08-07 13:46:13 +0000

    I provided the answers for tutorial

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,22 @@
 
 #include <stdio.h>
 
+//start main function
 int main() {
+
+	int mark1, mark2; //declaer varible
+	float avg; //declaer varible
+	
+	printf("Enter subject mark 1 : "); //get user input
+	scanf("%d", &mark1); //read integer
+	
+	printf("Enter subject mark 2 : "); //get user input
+	scanf("%d", &mark2); //read integer
+	
+	avg = (mark1 + mark2) / (float)2; //calculation
+	
+	printf("Average of the two marks : %.2f", avg); //printf avg
   
   return 0;
-}
+}//end main function
 
